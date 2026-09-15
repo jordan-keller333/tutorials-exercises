e85b0f8e40cb491917d73c4b976fd894c957bac8
Jordan Keller <jokeller@udel.edu>
2026-09-15 11:15:29 -0400

    exercise 2 finsihed

--- week-02/exercises/exercises-02-template.R
+++ week-02/exercises/exercises-02-template.R
@@ -11,8 +11,8 @@
 # 5. Do not delete the questions or instructions.
 # 6. Save, commit, and push your completed script.
 #
-# Name:
-# Date:
+# Name: jordan keller
+# Date: 9/15/26
 
 
 	#### Part 1: Objects and calculations ####
@@ -21,7 +21,9 @@
 	
 	# 1. Create an object named water_temp_c containing this value.
 
+water_temp_c <- 16.8
 
+water_temp_c
 
 	# 2. Convert the temperature to Fahrenheit using:
 	#
@@ -29,15 +31,18 @@
 	#
 	#    Save the result as water_temp_f.
 	
-	
+	water_temp_f <- water_temp_c * (9 / 5) + 32
+
+	water_temp_f
 	
 	# 3. Print water_temp_c and water_temp_f.
 	
+	print(water_temp_c) & print(water_temp_f)
 	
 	
 	# 4. Round water_temp_f to one decimal place.
 
-
+round(water_temp_f, digits = 1)
 
 	#### Part 2: Vectors and summary functions ####
 	
@@ -49,33 +54,36 @@
 	
 	# 1. The number of fish measured.
 	
-	
+	length(fish_lengths)
 	
 	# 2. The mean fish length.
 	
-	
+	mean(fish_lengths)
 	
 	# 3. The minimum fish length.
 
-
+min(fish_lengths)
 
 	# 4. The maximum fish length.
 	
-	
+max(fish_lengths)
 	
 	# 5. The number of fish longer than 32 cm.
 	
-	
+sum(fish_lengths > 32)
 	
 	# 6. Create a vector named fish_over_32 containing only fish
 	#    longer than 32 cm.
 	
-	
+	fish_over_32 <- (fish_lengths > 32)
+
+	fish_over_32
 	
 	# 7. Convert all fish lengths from centimeters to millimeters
 	#    and save the result as fish_lengths_mm.
 
-
+fish_lengths_mm <- fish_lengths * 10
+fish_lengths_mm
 
 	#### Part 3: Missing values ####
 	
@@ -85,20 +93,21 @@
 	
 	# 1. Count the number of missing values.
 	
-	
+	sum(is.na(fish_mass))
 	
 	# 2. Count the number of observed values.
 	
-	
+  	sum(!is.na(fish_mass))
 	
 	# 3. Calculate the mean mass while ignoring missing values.
 	#    Save the result as mean_fish_mass.
 	
-	
+	mean_fish_mass <- mean(fish_mass, na.rm = TRUE)
+	mean_fish_mass
 	
 	# 4. Calculate the total mass while ignoring missing values.
 	
-
+	sum(fish_mass, na.rm = TRUE)
 
 	#### Part 4: Build a data frame ####
 	
@@ -110,23 +119,23 @@
 	# caught:    TRUE, TRUE, FALSE, TRUE, TRUE, FALSE
 	
 	survey_data <- data.frame(
-	  station = c(),
-	  species = c(),
-	  length_cm = c(),
-	  caught = c()
+	  station = c("A", "B", "C", "D", "E", "F"),
+	  species = c("cod", "haddock", "cod", "hake", "haddock", "hake"),
+	  length_cm = c(41, 33, 38, 27, 36, 30),
+	  caught = c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE)
 	)
 	
 	# Inspect the data frame using each function below.
 	
-	# 1. str()
+str(survey_data)
 	
 	
 	
-	# 2. head()
+head(survey_data)
 	
 	
 	
-	# 3. summary()
+summary(survey_data)
 	
 	
 	
@@ -134,27 +143,29 @@
 	
 	# 1. What is the mean fish length?
 	
-	
+	mean(survey_data$length_cm)
 	
 	# 2. How many observations are cod?
 	
-	
+	sum(survey_data$species == "cod")
 	
 	# 3. How many fish were caught?
 
-
+sum(survey_data$caught)
 
 	# 4. Display all observations from fish at least 35 cm long.
 	
-	
+over_35 <- (survey_data[survey_data$length_cm > 35, ])
+over_35
 	
 	# 5. Display only the observations where a fish was caught.
 	
-	
+	caught_true <- (survey_data[survey_data$caught, ])
+	caught_true
 	
 	# 6. What is the mean length of the fish that were caught?
 	
-	
+	mean(caught_true$length_cm)
 	
 	#### Part 6: Debugging ####
 	
@@ -163,20 +174,20 @@
 	
 	# Error 1
 	
-	# mean_Length <- 35
-	# mean_length
+	mean_length <- 35
+mean_length
 	
 	
 	
 	# Error 2
 	
-	# fish_species <- c("cod", "haddock", hake")
-	
+fish_species <- c("cod", "haddock", "hake")
+	fish_species
 	
 	
 	# Error 3
 
-	# round(3.14159 digits = 2)
+round(3.14159, digits = 2)
 
 
 
@@ -190,8 +201,15 @@
 	#
 	# Then use large_and_caught to display the matching rows
 	# of survey_data.
-	
-	
+
+large_and_caught <- survey_data[
+  survey_data$length_cm > 35 & 
+    survey_data$caught == TRUE, 
+]
+
+large_and_caught
+
+
 	
 	#### Reflection ####
 	
@@ -199,6 +217,7 @@
 	# that became clearer during this exercise.
 	#
 	# Response:
+# I know how to do data frames better and also the challenge was helpful for similar exercises
 	
 	
 	#### Completion checklist ####
